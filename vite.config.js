7306b987b709268ce296ffdfa3afd98002b56335
BradleySawallRanken <Bradley_Sawall@insideranken.org>
2024-08-18 19:55:28 -0500

    ninth commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CRUD_ATTEMPT_9/',  // Ensure this matches your GitHub repo name
+  base: '/CRUD_ATTEMPT_9/',  
   plugins: [react()],
 });
