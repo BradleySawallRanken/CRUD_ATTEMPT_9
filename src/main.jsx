88ca8d33ca1625ae72d3bb21d393415e569861a4
BradleySawallRanken <Bradley_Sawall@insideranken.org>
2024-08-18 19:28:52 -0500

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import CRUDList from './components/CRUDList';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CRUDList />  {/* Render the main component */}
   </React.StrictMode>
 );
